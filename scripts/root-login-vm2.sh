e7103c7244ec53cc00368d6a572953ed453e5d11
Yanbo Zhou <yanbo.zyb@outlook.com>
2025-01-19 08:43:39 -0800

    scripts: add addtional VM 2

---
+++ scripts/root-login-vm2.sh
@@ -0,0 +1,1 @@
+ssh -p 2807 root@localhost
--- scripts/run-vm-dax2.sh
+++ scripts/run-vm-dax2.sh
@@ -3,23 +3,23 @@
 # image directory
 IMGDIR=$HOME/images
 # Virtual machine disk image
-OSIMGF=$IMGDIR/u20s.qcow2
+OSIMGF=$IMGDIR/u20s2.qcow2
 # qemu binary
 QEMU_BIN=/home/yaz093/qemu/build/qemu-system-x86_64
-CPU_BIND='taskset -c 0-19' 
+CPU_BIND='taskset -c 40-59' 
 
 sudo $CPU_BIND $QEMU_BIN \
-    -name "Test-VM" \
+    -name "VM-T2" \
     -enable-kvm \
     -cpu host \
     -smp 20 \
-    -m 8G \
-    -object memory-backend-file,id=mem1,share=on,mem-path=/dev/dax0.0,size=8G,align=2M \
+    -m 36G \
+    -object memory-backend-file,id=mem1,share=on,mem-path=/dev/dax0.0,size=36G,align=2M,offset=8G \
     -numa node,memdev=mem1 \
     -device virtio-scsi-pci,id=scsi0 \
     -device scsi-hd,drive=hd0 \
     -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
-    -net user,hostfwd=tcp::2806-:22 \
+    -net user,hostfwd=tcp::2807-:22 \
     -net nic,model=virtio \
     -nographic \
     -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
