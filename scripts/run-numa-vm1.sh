3be36953c9e9e8fe00ca8368a930f143de8a00c3
Yanbo Zhou <yanbo.zyb@outlook.com>
2025-03-20 10:26:57 -0700

    update script

--- scripts/run-numa-vm1.sh
+++ scripts/run-numa-vm1.sh
@@ -12,7 +12,7 @@ CPU_BIND_20='taskset -c 20-29,60-69'
 CPU_BIND_24='taskset -c 20-31,60-71'
 CPU_BIND_40='taskset -c 20-39,60-79'
 
-sudo $CPU_BIND_24 $QEMU_BIN \
+sudo $CPU_BIND_40 $QEMU_BIN \
     -name "VM-NUMA-1" \
     -enable-kvm \
     -cpu host \
@@ -21,7 +21,7 @@ sudo $CPU_BIND_24 $QEMU_BIN \
     -object memory-backend-file,id=mem0,share=on,mem-path=/dev/dax0.0,size=20G,align=2M \
     -object memory-backend-file,id=mem1,share=on,mem-path=/dev/dax1.0,size=20G,align=2M \
     -object memory-backend-file,id=mem2,share=on,mem-path=/dev/dax1.0,size=20G,align=2M,offset=20G \
-    -numa node,nodeid=0,memdev=mem0,cpus=0-23,tier-id=0,dax-id=0,seg-id=0 \
+    -numa node,nodeid=0,memdev=mem0,cpus=0-39,tier-id=0,dax-id=0,seg-id=0 \
     -numa node,nodeid=1,memdev=mem1,tier-id=0,dax-id=1,seg-id=0 \
     -numa node,nodeid=2,memdev=mem2,tier-id=0,dax-id=1,seg-id=1 \
     -device virtio-scsi-pci,id=scsi0 \
@@ -30,4 +30,8 @@ sudo $CPU_BIND_24 $QEMU_BIN \
     -net user,hostfwd=tcp::2806-:22 \
     -net nic,model=virtio \
     -nographic \
-    -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
+    -qmp unix:./qmp-sock,server,nowait \
+    -chardev socket,path=./qga-sock,server=on,wait=off,id=qga0 \
+    -device virtio-serial \
+    -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0 \
+    2>&1 | tee log
